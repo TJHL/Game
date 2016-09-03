f4b0346032da82e8b1ce19ff68406188727b31f4
TJHL <league@WTS9.attlocal.net>
2016-09-03 14:07:02 -0700

    Game started

---
+++ Game/Assets/Scripts/InventoryScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class InventoryScreen : MonoBehaviour {
+	GameObject Canvas;
+	void Awake(){
+		Canvas = GameObject.Find ("Canvas");
+		Canvas.SetActive (false);
+	}
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.G)) {
+			Canvas.SetActive (!Canvas.activeInHierarchy);
+			UnityStandardAssets.Characters.FirstPerson.MouseLook.CamMouseMove = !UnityStandardAssets.Characters.FirstPerson.MouseLook.CamMouseMove;
+
+		}
+	}
+
+}
